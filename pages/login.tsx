b579cd35f866cdca56dbb3d7a89b029cab3bf9ae
Shadrach Chuks <80325389+danielChuks@users.noreply.github.com>
2022-09-08 15:14:26 +0000

    react hook form

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,27 @@
 import Head from "next/head";
 import Image from 'next/image';
+import { useState } from "react";
+import {SubmitHandler, useForm} from "react-hook-form";
+
+// creating a type script input type 
+
+interface Inputs {
+  email: string,
+  password: string
+}
 
 function Login() {
+  const [Login, setLogin] =useState(false);
+
+  const { register, 
+          handleSubmit, 
+          watch, 
+          formState: { errors } 
+      } = useForm<Inputs>();
+      const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+
   return (
-    <div className="relative flex h-screen w-screen flex-col bg-black md:item-center md: justify-center  md:bg-transparent">
+    <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center  md:bg-transparent">
       <Head>
         <title> Netflix </title>
         <link rel="icon" href="/favicon.ico" />
@@ -19,15 +37,20 @@ function Login() {
           className="absolute left-4 top-4 cursor-pointer md:left-8 md:top-4"
           src="https://rb.gy/ek4j9f" width={150} height={150} />
 
-        <form className="relative rounded mt-24 space-y-8 bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
-          <h1 className=""> Sign in </h1>
+        <form onSubmit={handleSubmit(onSubmit)} className="relative rounded mt-24 space-y-8 bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+          <h1 className="text-4xl font-semibold"> Sign in </h1>
             <div className="space-y-4">
-             <label>
+             <label className="inline-block w-full">
                 <input className="input" type="email" placeholder="Email"/>
               </label>
-             <label>
-                <input className="input" type="password" placeholder="password"/>
+             <label className="inline-block w-full">
+                <input className="input" type="password" placeholder="Password"/>
              </label>
+          </div>
+          <button className="w-full rounded bg-[#e50914] py-3"> Sign in </button>
+          <div className="text-[gray]">
+            New to Netflix ? {''}
+            <button type="submit"  className="text-white hover:underline">Sign up now</button>
           </div>
         </form>
       </div>
