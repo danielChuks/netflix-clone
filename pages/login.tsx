ac445c28bd29b5166337b05fcd8f972a31648216
Shadrach Chuks <80325389+danielChuks@users.noreply.github.com>
2022-09-08 13:29:38 +0000

    login route

--- pages/login.tsx
+++ pages/login.tsx
@@ -19,15 +19,14 @@ function Login() {
           className="absolute left-4 top-4 cursor-pointer md:left-8 md:top-4"
           src="https://rb.gy/ek4j9f" width={150} height={150} />
 
-        <form className="relative rounded mt-24 space-y-8 bg-black/75 py-10 px-10 md:top">
-          <h1> Sign in</h1>
-            <div>
+        <form className="relative rounded mt-24 space-y-8 bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+          <h1 className=""> Sign in </h1>
+            <div className="space-y-4">
              <label>
-                <input className="" type="email" placeholder="Email" />
+                <input className="input" type="email" placeholder="Email"/>
               </label>
-
              <label>
-              
+                <input className="input" type="password" placeholder="password"/>
              </label>
           </div>
         </form>
