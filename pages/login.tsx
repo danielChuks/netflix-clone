3aecbae678d53887876186d472bd048ff9001541
Daniel Chuks <danieluchechukwuchuks1@gmail.com>
2022-09-09 12:05:00 +0100

    form validation

--- pages/login.tsx
+++ pages/login.tsx
@@ -11,11 +11,10 @@ interface Inputs {
 }
 
 function Login() {
-  const [Login, setLogin] =useState(false);
+  const [login, setLogin] =useState(false);
 
   const { register, 
           handleSubmit, 
-          watch, 
           formState: { errors } 
       } = useForm<Inputs>();
       const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
@@ -32,19 +31,34 @@ function Login() {
         className="-z-10 !hidden opacity-60 sm:!inline"
         objectFit="cover"
       />
-
         <img 
           className="absolute left-4 top-4 cursor-pointer md:left-8 md:top-4"
           src="https://rb.gy/ek4j9f" width={150} height={150} />
 
-        <form onSubmit={handleSubmit(onSubmit)} className="relative rounded mt-24 space-y-8 bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+        <form 
+            onSubmit={handleSubmit(onSubmit)} 
+            className="relative rounded mt-24 space-y-8 bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
           <h1 className="text-4xl font-semibold"> Sign in </h1>
             <div className="space-y-4">
              <label className="inline-block w-full">
-                <input className="input" type="email" placeholder="Email"/>
-              </label>
+                <input 
+                  {...register("email", {required: true})}
+                  className="input" 
+                  type="email" 
+                  placeholder="Email"/>
+                  {errors.email && (
+                    <p className="p-1 text-[13px] font-inline text-orange-500">Invalid email submission</p>
+                    )}
+              </label>  
              <label className="inline-block w-full">
-                <input className="input" type="password" placeholder="Password"/>
+                <input 
+                  {...register("password", {required: true})}
+                  className="input" 
+                  type="password" 
+                  placeholder="Password"/>
+                  {errors.password && ( 
+                    <p className="p-1 text-[13px] font-inline text-orange-500">Inavild password submission</p>
+                  )}
              </label>
           </div>
           <button className="w-full rounded bg-[#e50914] py-3"> Sign in </button>
