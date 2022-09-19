5d5d9922ee5b63679e8d0133781a337634238874
Shadrach Chuks <80325389+danielChuks@users.noreply.github.com>
2022-09-19 18:25:54 +0000

    player injection on modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,36 +1,44 @@
 import { XMarkIcon } from '@heroicons/react/24/outline'
 import MuiModal from '@mui/material/Modal'
 import { useEffect, useState } from 'react'
+import ReactPlayer from 'react-player'
 import { useRecoilState } from 'recoil'
 import { modalState, movieState } from '../atoms/modalAtom'
-import { Movie } from '../typings'
-
+import { Element, Genre } from '../typings'
 
 function Modal() {
     const [showModal, setShowModal ] = useRecoilState(modalState)
     const [movie, setMovie] = useRecoilState(movieState)
-    const [data, setData] = useState()
+    const [trailer, setTrailer] = useState("")
+    const [genres, setGenres] = useState<Genre[]>([])
+
 
     ///This useEffect is going to ensure we fetch the videos
-    useEffect (() => {
-        if(!movie) return
+   useEffect(() => {
+    if (!movie) return
 
-        async function fetchMovie(){
-            const data = await fetch(`https://api.themoviedb.org/3/${
-                movie?.media_type === 'tv' ? 'tv' : 'movie'
-              }/${movie?.id}?api_key=${
-                process.env.NEXT_PUBLIC_API_KEY
-              }&language=en-US&append_to_response=videos`
-            )
-            .then((response) => response.json())
-            .catch((error) => console.log(error.message))
-            
-            setData(data)
-            
-        }
-        fetchMovie()
-    }, [movie])
-    console.log(data);
+    async function fetchMovie() {
+      const data = await fetch(
+        `https://api.themoviedb.org/3/${
+          movie?.media_type === 'tv' ? 'tv' : 'movie'
+        }/${movie?.id}?api_key=${
+          process.env.NEXT_PUBLIC_API_KEY
+        }&language=en-US&append_to_response=videos`
+      ).then((response) => response.json())
+      if (data?.videos) {
+        const index = data.videos.results.findIndex(
+          (element: Element) => element.type === 'Trailer'
+        )
+        setTrailer(data.videos?.results[index]?.key)
+      }
+      if (data?.genres) {
+        setGenres(data.genres)
+      }
+    }
+
+    fetchMovie()
+  }, [movie])
+  console.log(trailer)
 
     //helper function that willhelp us close the modal........
     const handleClose = () => {
@@ -44,12 +52,19 @@ function Modal() {
                 <XMarkIcon />
             </button>
             <div>
-
+                <ReactPlayer
+                url={`https://www.youtube.com/watch?v=${trailer}`}
+                width="100%"
+                height="100%"
+                style={{ position: 'absolute', top: '0', left: '0' }}
+                playing
+                // muted={muted}
+              />
             </div>
         </>
     </MuiModal>
   )
 }
 
-export default Modal
+export default Modal;
 
--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,15 +1,25 @@
 import { Movie } from "../typings";
 import Image from 'next/image';
+import {useRecoilState} from 'recoil';
+import { modalState, movieState } from '../atoms/modalAtom'
+
 
 interface Props{
     movie: Movie
 }
 
 function Thumbnail({movie}: Props) {
+  const [showModal, setShowModal ] = useRecoilState(modalState)
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
   return (
     <div 
+      onClick={() => { 
+        setCurrentMovie(movie), 
+        setShowModal(true)
+      }}
       className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out m:h-36 md:min-w-[260px] md:hover:scale-105">
-        <Image src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path || movie.poster_path}`}
+        <Image 
+        src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path || movie.poster_path}`}
         layout="fill"
         className="rounded-sm object-cover md:rounded"
         />
@@ -17,4 +27,4 @@ function Thumbnail({movie}: Props) {
   )
 }
 
-export default Thumbnail
+export default Thumbnail;
