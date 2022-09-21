3f5500e04a9e324f8d5c895aaaa1741cfc5bb8f0
Shadrach Chuks <80325389+danielChuks@users.noreply.github.com>
2022-09-21 15:05:58 +0000

    modal display div

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -92,8 +92,8 @@ function Modal() {
               </div>
           </div>
 
-          <div>
-                <div>
+          <div className='flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8'>
+                <div className='space-y-6 text-lg'>
                     <div className="flex items-center space-x-2 text-sm">
                         <p className="font-semibold text-green-400">
                           {movie!.vote_average * 10} % March
